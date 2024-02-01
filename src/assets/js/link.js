fda433a5214b093026d58fd08e43654d1b2ed322
kimchanbi2001 <cksql3634@gmail.com>
2024-02-02 00:35:47 +0900

    🥳 레이아웃 완성

---
+++ src/assets/js/link.js
@@ -0,0 +1,12 @@
+export function link() {
+    console.log(link)
+}
+export function menu() {
+    console.log(menu)
+}
+export function port() {
+    console.log(port)
+}
+export function smooth() {
+    console.log(smooth)
+}
