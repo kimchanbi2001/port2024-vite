0adeb23729f7bb7a8a6caf5b90d7a0aea66f6d04
kimchanbi2001 <cksql3634@gmail.com>
2024-02-03 23:31:12 +0900

    🥳 해더 완성

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,12 +1,3 @@
 export function link() {
     console.log(link)
 }
-export function menu() {
-    console.log(menu)
-}
-export function port() {
-    console.log(port)
-}
-export function smooth() {
-    console.log(smooth)
-}
--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,10 @@
-export function menu(){
-    console.log(menu)
-}
+export function menu() {
+    const btnEl = document.querySelector('.top_arr');
+
+    btnEl.addEventListener('click', () => {
+      window.scrollTo({
+        top:0,
+        behavior:'smooth'
+      });
+    });
+};
